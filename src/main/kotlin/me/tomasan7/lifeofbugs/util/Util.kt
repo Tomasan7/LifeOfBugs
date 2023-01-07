8ed764ac703d2903dd8d9b90789addbd4c497a7b
Tomasan7 <tomashula06@gmail.com>
2023-01-07 13:48:20 +0100

    Fix names.txt file not being recognized

--- src/main/kotlin/me/tomasan7/lifeofbugs/util/Util.kt
+++ src/main/kotlin/me/tomasan7/lifeofbugs/util/Util.kt
@@ -1,12 +1,10 @@
 package me.tomasan7.lifeofbugs.util
 
 import androidx.compose.ui.graphics.Color
-import me.tomasan7.lifeofbugs.game.GameConfig
-import java.io.File
 import kotlin.math.ceil
 import kotlin.random.Random
 
-fun randomName() = namesFile.readLines().random()
+fun randomName() = names.random()
 
 fun randomColor() = Color(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
 
@@ -18,4 +16,4 @@ fun Float.ceil() = ceil(this).toInt()
 
 fun Float.floor() = this.toInt()
 
-private val namesFile = File(Thread.currentThread().contextClassLoader.getResource("names.txt").file)
+private val names = Thread.currentThread().contextClassLoader.getResource("names.txt").readText().split("\n")
